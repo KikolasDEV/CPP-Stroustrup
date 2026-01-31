6fed4c5a2fcc5cfd7445c3578841a9393dcb36e1
KikolasDEV <kikoamarillo94@gmail.com>
2026-01-31 21:57:05 +0100

    Añadir función para mantener la ventana de consola abierta en "Hello World"

--- chapter02/hello_world.cpp
+++ chapter02/hello_world.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!\n";
+void keep_window_open() {               // function to keep the console window open
+    std::cout << "Press ENTER to continue...";
+    std::cin.get();
+}
+
+int main() {                            // C++ programs start by executing the function main
+    std::cout << "Hello World!\n";     // output "Hello World!" 
+    keep_window_open();               // wait for a character to be entered (ENTER)
     return 0;
 }
